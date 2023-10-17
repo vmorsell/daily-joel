4bc480817d9b9737c67b4a3f9c3f1725020ddab5
Viktor Mörsell <viktor@upper.st>
2023-10-17 22:01:55 +0200

    Add 2023-10-17

---
+++ 2023-10-17/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func concatenate(a, b string) string {
+	return a + b
+}
+
+func swap(a, b string) (x, y string) {
+	x = b
+	y = a
+	return
+}
+
+func main() {
+	a := "el"
+	b := "Jo"
+
+	a, b = swap(a, b)
+
+	fmt.Println(concatenate(a, b))
+}
