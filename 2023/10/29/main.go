2ad9c35237e19ec72ff946bb67ebb262baa84505
Viktor Mörsell <viktor@upper.st>
2023-10-29 09:44:56 +0100

    Add 2023-10-29

---
+++ 2023/10/29/main.go
@@ -0,0 +1,25 @@
+// Daily Joel 2023-10-29
+//
+// Methods.
+package main
+
+import "fmt"
+
+type Person struct {
+	name string
+}
+
+func (p *Person) setName(name string) {
+	p.name = name
+}
+
+func (p Person) setNameWithoutEffect(name string) {
+	p.name = name
+}
+
+func main() {
+	p := Person{}
+	p.setName("Joel")
+	p.setNameWithoutEffect("Someone else")
+	fmt.Println(p.name)
+}
