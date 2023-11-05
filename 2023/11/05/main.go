abf99cb1b33901c888e2c2a01b0456f3b8c63eb5
Viktor Mörsell <viktor@upper.st>
2023-11-05 01:05:01 +0100

    Add 2023-11-05

---
+++ 2023/11/05/main.go
@@ -0,0 +1,17 @@
+// Daily Joel 2023-11-05
+//
+// Maps and zero values.
+package main
+
+import "fmt"
+
+func main() {
+	names := map[int]string{
+		2: "Joel",
+		4: "\n",
+	}
+
+	for k := 0; k < 10; k++ {
+		fmt.Print(names[k])
+	}
+}
